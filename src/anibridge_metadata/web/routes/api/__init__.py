02564bd202374bc2d7da8edc42aa4acec516898e
Elias Benbourenane <eliasbenbourenane@gmail.com>
2026-04-12 01:22:50 +0000

    feat: Database dump endpoint

--- src/anibridge_metadata/web/routes/api/__init__.py
+++ src/anibridge_metadata/web/routes/api/__init__.py
@@ -2,10 +2,12 @@
 
 from fastapi import APIRouter
 
+from anibridge_metadata.web.routes.api.dump import router as dump_router
 from anibridge_metadata.web.routes.api.metadata import router as metadata_router
 
 __all__ = ["router"]
 
 router = APIRouter()
 
+router.include_router(dump_router, prefix="/dump", tags=["dump"])
 router.include_router(metadata_router, prefix="/metadata", tags=["metadata"])
